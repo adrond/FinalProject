b95f377611a19771633f7c47b86c013b9d104afe
PotapovAY <adrond@mail.ru>
2023-11-09 18:59:51 +0300

    Initial commit

---
+++ src/test/java/com/adrond/finalproject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrond.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
